6d7f7252ae2b5235ed43881b7f626a059cbbf411
Ecalose <47114714+Ecalose@users.noreply.github.com>
2021-06-23 00:36:19 +0800

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,10 +3,8 @@ echo '====^_^===='
 echo '欢迎使用佰阅发卡(KAMIFAKA)程序'
 
 
-if [ ${DB_TYPE} = 'Mysql' ];then
-   sed -i "s|'sqlite:///'+os.path.join(SQL_PATH,'kamifaka.db')|'mysql+pymysql://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_DATABASE}\?charset=utf8mb4'|g" /usr/src/app/service/api/db.py
-elif [ ${DB_TYPE} = 'PostgreSQL' ];then
-   sed -i "s|'sqlite:///'+os.path.join(SQL_PATH,'kamifaka.db')|'postgresql+psycopg2://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_DATABASE}'|g" /usr/src/app/service/api/db.py
+if [ ${DATABASE_TYPE} = 'Mysql' ];then
+    sed -i "s/'sqlite:\/\/\/'+os.path.join(SQL_PATH,'kamifaka.db')/'mysql+pymysql:\/\/${MYSQL_USER}:${MYSQL_PASSWORD}@${MYSQL_HOST}:${MYSQL_PORT}\/${MYSQL_DATABASE}\?charset=utf8mb4'/g" /usr/src/app/service/api/db.py
 fi
 
 # 处理文件夹
@@ -29,9 +27,6 @@ fi
 # 然后初始化数据库
 python init_mysql.py
 
-# TG发卡后台任务
-#python app_tg.py >/dev/null 2>&1 &
-
 echo '程序初始化完成'
 # ["gunicorn","-k", "gevent", "--bind", "0.0.0.0:8000", "--workers", "8", "app:app"]
-gunicorn -k gevent --bind 0.0.0.0:8000 --workers $(( 2 * `cat /proc/cpuinfo | grep 'core id' | wc -l` + 1 )) --log-level critical app:app
+gunicorn -k gevent --bind 0.0.0.0:8000 --workers 8 app:app
